ba649e466f2bcebb69563b6215bc9f0004ca7b0b
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-12-13 14:26:45 -0600

    247. Decoding Data with NSCoder

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -18,23 +18,7 @@ class TodoListViewController: UITableViewController {
 	// MARK: - UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-		let newItem = Item()
-		newItem.title = "Find Mike"
-		newItem.done = true
-		itemArray.append(newItem)
-		
-		let newItem2 = Item()
-		newItem2.title = "Buye Eggos"
-		itemArray.append(newItem2)
-		
-		let newItem3 = Item()
-		newItem3.title = "Destroy Demogorgons"
-		itemArray.append(newItem3)
-		
-//		if let safeItems = defaults.array(forKey: defaultsKey) as? [Item] {
-//			itemArray = safeItems
-//		}
+		loadItems()
     }
 	
 	// MARK: - TableView DataSource
@@ -87,6 +71,17 @@ class TodoListViewController: UITableViewController {
 			try data.write(to: dataFilePath!)
 		} catch {
 			print("Error encoding item array: \(error)")
+		}
+	}
+	
+	private func loadItems() {
+		if let data = try? Data(contentsOf: dataFilePath!) {
+			let decoder = PropertyListDecoder()
+			do {
+				itemArray = try decoder.decode([Item].self, from: data)
+			} catch {
+				print("Error decoding item array: \(error)")
+			}
 		}
 	}
 }
